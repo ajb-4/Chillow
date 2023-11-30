a5dfb862a5224b4a45a4654f9255f86ea2cd46fc
ajb-4 <alexbrown4711@gmail.com>
2023-11-30 10:06:32 -0500

    resolve Git issue

--- frontend/src/components/HomeShow/index.js
+++ frontend/src/components/HomeShow/index.js
@@ -178,10 +178,7 @@ const HomeShow = ({homeId}) => {
                         </div>
                         <div id='homeshow-overview'>
                             <div id='homeshow-overviewtext'>Overview</div>
-                            <div id={isExpanded ? 'homeshow-description-expanded' : 'homeshow-description'}>Soon Lara spotted another landmarks—an outcropping of limestone beside the path that had a silhouette like a man’s face, a marshy spot beside the river where the waterfowl were easily startled, a tall tree that looked like a man with his arms upraised. They were drawing near to the place where there was an island in the river. The island was a good spot to make camp. They would sleep on the island tonight.
-Lara had been back and forth along the river path many times in her short life. Her people had not created the path—it had always been there, like the river—but their deerskin-shod feet and the wooden wheels of their handcarts kept the path well worn. Lara’s people were salt traders, and their livelihood took them on a continual journey.
-At the mouth of the river, the little group of half a dozen intermingled families gathered salt from the great salt beds beside the sea. They groomed and sifted the salt and loaded it into handcarts. When the carts were full, most of the group would stay behind, taking shelter amid rocks and simple lean-tos, while a band of fifteen or so of the heartier members set out on the path that ran alongside the river.
-With their precious cargo of salt, the travelers crossed the coastal lowlands and traveled toward the mountains. But Lara’s people never reached the mountaintops; they traveled only as far as the foothills. Many people lived in the forests and grassy meadows of the foothills, gathered in small villages. In return for salt, these people would give Lara’s people dried meat, animal skins, cloth spun from wool, clay pots, needles and scraping tools carved from bone, and little toys made of wood.</div>
+                            <div id={isExpanded ? 'homeshow-description-expanded' : 'homeshow-description'}>{home.description}</div>
                             <div id='toggleexpanddescription' onClick={toggleExpand}>{!isExpanded ? 'Show more' : 'Hide'}</div>
                             <div id='homepage-listingstats'>
                                 <div id='homeshow-timeonzillow'>
